981cb0b8cf4d138929278f6a0c3f73a22beaae17
Aaron Vega <me@vorticist.net>
2021-09-21 10:58:43 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+############################
+# Final container
+############################
+FROM registry.cto.ai/official_images/node:2-12.13.1-stretch-slim
+
+WORKDIR /ops
+
+BLAH
+
+RUN npm install fastify
+RUN npm install fastify-plugin
+RUN npm install fastify-cli
+RUN npm install	fastify-autoload
+
+ADD . .
+RUN chown -R ops:9999 /ops /home/ops
---
+++ index.js
@@ -0,0 +1,16 @@
+const { sdk } = require('@cto.ai/sdk')
+const http = require('http')
+
+async function main() {
+  const requestListener = function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.write(JSON.stringify(JSON.stringify({"message": "HEROKU is the best of the best"})))
+    res.end()
+  }
+
+  const server = http.createServer(requestListener)
+  server.listen(8080, function () {
+    sdk.log('server has begun to serve!')
+  })
+}
+main()
