a69389de9cf79aaf77aaabb6dec134d8c26aaf72
Aaron Vega <aaron@cto.ai>
2021-12-08 16:21:36 -0800

    added more code

--- code/main.go
+++ code/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	for i := 0; i <= 9; i++ {
+		fmt.Print(strconv.Itoa(fibonacciRecursion(i)) + " ")
+	}
+	fmt.Println("")
+}
+
+func fibonacciRecursion(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacciRecursion(n-1) + fibonacciRecursion(n-2)
 }
